ac3874f2600dd3e7b1fd5002aec2be5c2ddce18b
Vinitha-Nayak <nayakvinitha413@gmail.com>
2023-12-26 16:52:36 +0530

    addedd files

---
+++ vpc_creation/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "ap-northeast-1"
+  
+}
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
