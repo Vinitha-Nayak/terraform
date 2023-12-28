d64228e3ec36ac86c1dbb61619501867651ec973
Vinitha <151067108+Vinitha-Nayak@users.noreply.github.com>
2023-12-28 20:32:36 +0530

    Add files via upload

---
+++ terraform_modules/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance"{
+  source = "./modules/ec2_instance"
+  ami_value  = "ami-079db87dc4c10ac91"
+   instance_type_value = "t2.micro"
+}
